b8dd9876555f16fa81801ad0d8264d823581ce18
3mar-04 <omar_sabri2004@icloud.com>
2024-09-23 15:49:57 +0300

    Slide-Puzzle

---
+++ puzzle.js
@@ -0,0 +1,82 @@
+var rows = 3;
+var columns = 3;
+
+var currTile;
+var otherTile;
+
+let turns = 0;
+var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function shuffleArray(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+imgOrder = shuffleArray(imgOrder);
+
+window.onload = () => {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      let tile = document.createElement("img");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.src = imgOrder.shift() + ".jpg";
+
+      tile.addEventListener("dragstart", dragStart);
+      tile.addEventListener("dragover", dragOver);
+      tile.addEventListener("dragenter", dragEnter);
+      tile.addEventListener("dragleave", dragLeave);
+      tile.addEventListener("drop", dragDrop);
+      tile.addEventListener("dragend", dragEnd);
+
+      document.getElementById("puzzle").append(tile);
+    }
+  }
+};
+
+function dragStart() {
+  currTile = this;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+}
+
+function dragLeave() {}
+
+function dragDrop() {
+  otherTile = this;
+}
+
+function dragEnd() {
+  if (!otherTile.src.includes("1.jpg")) {
+    return;
+  }
+  let currCoords = currTile.id.split("-");
+  let r = parseInt(currCoords[0]);
+  let c = parseInt(currCoords[1]);
+
+  let otherCoords = otherTile.id.split("-");
+  let r2 = parseInt(otherCoords[0]);
+  let c2 = parseInt(otherCoords[1]);
+
+  let moveLeft = r == r2 && c2 == c - 1;
+  let moveRight = r == r2 && c2 == c + 1;
+
+  let moveUp = c == c2 && r2 == r - 1;
+  let moveDown = c == c2 && r2 == r + 1;
+
+  let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+  if (isAdjacent) {
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+    turns++;
+    document.getElementById("counter").textContent = turns;
+  }
+}
